7b271f142db8f2758a6f59293c2ca81ae16d50dd
Mark Cleo Calbang <markcleocalbang22@gmail.com>
2023-10-01 22:35:04 +0800

    modify

---
+++ Function/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		printMessage();
+        sum( 5, 2);
+	}
+    public static void printMessage(){
+         System.out.println("Hellow! Miss");
+    } 
+    public static void sum(int a  , int b){
+        int sum = a + b;  
+       System.out.println(sum);  
+        
+    }
+}
