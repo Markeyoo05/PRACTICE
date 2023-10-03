2c116410619f9c5f0128346a6d18508caab2c8f2
Mark Cleo Calbang <markcleocalbang22@gmail.com>
2023-10-03 19:07:07 +0800

    Add

---
+++ Love/src/Love.java
@@ -0,0 +1,19 @@
+public class Love
+{ 
+    public static void Love(){
+         int n = 4 , i , j ; 
+         
+         for ( i =0; i < n ; i++){
+             for ( j = 0 ; j < i ; j++){
+                 
+                   System.out.print("   ");
+             }
+             System.out.println("I love programming");
+         }
+        
+    }
+	public static void main(String[] args)
+	{
+		 Love();
+	}
+}
