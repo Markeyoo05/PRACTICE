d97335e2c73bfba3331f18f7750a1e1d1e727065
Mark Cleo Calbang <markcleocalbang22@gmail.com>
2023-10-12 11:53:19 +0800

    modified

--- Chars/src/Main.java
+++ Chars/src/Main.java
@@ -10,16 +10,14 @@ public class Main {
                 char char2 = scanner.next().charAt(0);
                 System.out.print("Enter the third character: ");
                 char char3 = scanner.next().charAt(0);
-
-                // Call the combineChars function to combine the characters
                 String combined = combineChars(char1, char2, char3);
                 System.out.println("Combined result: " + combined);
 
             }
 
-        // Function to combine three characters
+ 
         public static String combineChars(char char1, char char2, char char3) {
-                // Combine the characters and return the result as a string
+               
                 return String.valueOf(char1) + String.valueOf(char2) + String.valueOf(char3);
             }
     }
